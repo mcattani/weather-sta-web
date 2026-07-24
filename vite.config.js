c78cba615f9e1fab50a1be353affe32c4a5d8389
mcattani <marianocattani@gmail.com>
2026-07-24 13:42:35 -0300

    corregido bug de versión

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import packageJson from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: { 
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version) 
-  }
-})
+  define: {
+    // Inyecta la versión de package.json en la constante __APP_VERSION__
+    __APP_VERSION__: JSON.stringify(packageJson.version),
+  },
+});
