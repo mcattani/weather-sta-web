e2cc272ce40879e29a17acf4afedbf5315be298a
mcattani <marianocattani@gmail.com>
2026-07-29 11:41:24 -0300

    corrección de código para deploy en vercel

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import packageJson from './package.json';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: ['es2022', 'chrome100', 'safari15', 'firefox100'],
+    cssTarget: ['chrome100', 'safari15', 'firefox100'],
+  },
   define: {
     // Inyecta la versión de package.json en la constante __APP_VERSION__
     __APP_VERSION__: JSON.stringify(packageJson.version),
