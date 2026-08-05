21dc4465ee30e2059b17faf490b98b4133f7e64a
mcattani <marianocattani@gmail.com>
2026-08-05 11:46:05 -0300

    prueba links header, botón hamburguesa

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import { CloudSunFill } from 'react-bootstrap-icons';
 
 export default function Header() {
@@ -6,17 +6,31 @@ export default function Header() {
         <Navbar
             expand="lg"
             className="glass-navbar mb-4 py-3 sticky-top"
+            collapseOnSelect
         >
             <Container>
+
+                {/* Logo y título */}
                 <Navbar.Brand href="#" className="d-flex align-items-center gap-2 fw-bold mb-0">
                     <CloudSunFill size={26} style={{ color: '#FFB74D' }} />
                     <span className="display-font">Weather STA</span>
                     <span className="eyebrow ms-1">TNA</span>
                 </Navbar.Brand>
+
+                {/* Botón Hamburguesa */}
+                <Navbar.Toggle aria-controls="navbarScroll" />
+
+                {/* Contenido colapsable */}
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className="me-auto display-font">
+                        <Nav.Link href="#" className="nav-link fw-bold">
+                            Inicio
+                        </Nav.Link>
+                        {/* Próximos links */}
+                    </Nav>
+                </Navbar.Collapse>
+
             </Container>
         </Navbar>
     );
 }
-
-
-
